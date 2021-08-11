b947c3da723308ad193274ad3e15fdcf5d3f3a07
broxzhang <broxzhang@gmail.com>
2021-08-10 22:14:23 -0400

    update project

--- js/seafood.js
+++ js/seafood.js
@@ -1,5 +1,5 @@
 // const seafood = [
-//     {    
+//     {
 //         id: 1,
 //         productName: 'Fresh Canadian Atlantic Salmon Fillet',
 //         aisles: 'seafood',
@@ -9,7 +9,7 @@
 //         avg: 7.93,
 //         photo: 'img/salmon_fillet.jpeg'
 //     },
-//     {    
+//     {
 //         id: 2,
 //         productName: 'Crab flavoured pollock flakes',
 //         aisles: 'seafood',
@@ -19,8 +19,8 @@
 //         avg: null,
 //         photo: 'img/crab-flavored.jpeg'
 //     },
-//     {    
-//         id: 3, 
+//     {
+//         id: 3,
 //         productName: 'Fresh Atlantic Salmon Steak',
 //         aisles: 'seafood',
 //         price: 15.99,
@@ -31,88 +31,112 @@
 //     }
 // ]
 
-import seafood from './product.js';
+import products from "./product.js";
 // import seafood from './product.js';
 
+const seafoodSection = document.querySelector("#seafoodSection");
 
-const seafoodSection = document.querySelector(".products-container");
+const { seafood } = products;
 
-window.addEventListener("DOMContentLoaded", function(){
-    displayFood(seafood)
-})
+console.log(seafood);
 
-function displayFood(seafood) {
-    let displayFood = seafood.map( (item) => {
-        const pizKg = calculateKg(item.price);
+window.addEventListener("DOMContentLoaded", function () {
+  displayFood(seafood);
+});
 
-        const avgStr = item.avg? item.avg + ' avg. ea. ' : (pizKg/10).toFixed(2) + '/100g';
-        return `
-        <a style="cursor: pointer;" class="product" href="seafood-detail.html">
-        <img class="product-img" src="${item.photo}" />
-        <h3>${item.productName}</h3>
-        <h4><b>${avgStr}</b></h4>
-        <span>(200 g avg.)</span>
-        <span>${pizKg}/kg ${item.price}/lb</span>
-        <!-- <img style="width: 40px; height: 40px; color: black;" src="shopping_cart.png" /> -->
-        <i class="fas fa-shopping-cart"></i>
-    </a>`
-    })
-    displayFood = displayFood.join("");
-    seafoodSection.innerHTML = displayFood;
-}
+const displayFood = (seafood) => {
+  let displayFood = seafood.map((item, i) => {
+    const pizKg = calculateKg(item.price);
 
-function calculateKg(price) {
+    const avgStr = item.avg
+      ? item.avg + " avg. ea. "
+      : (pizKg / 10).toFixed(2) + "/100g";
+    return `
+  <div class="card col-md-4" style="min-width: 250px">
+        <a href="seafood-detail.html?itemid=${item.id}">
+        <img class="card-img-top" src="${item.photo}" alt="seafood image" >
+        </a>
+        <div class="card-body">
+          <h5 class="card-title">${item.productName}</h5>
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item">${avgStr}</li>
+            <li class="list-group-item">${pizKg}/kg ${item.price}/lb</li>
+          </ul>
+          </div>
+        <div class="card-body">
+        <a href="shoppingCart.html?itemid=${item.id}"> 
+          <i class="fas fa-shopping-cart">Add To Cart</i>
+        </a>
+        </div>
+      </div>`;
+  });
+  displayFood = displayFood.join("");
+  seafoodSection.innerHTML = displayFood;
+
+  function calculateKg(price) {
     return (price * 2.205).toFixed(2);
-}
+  }
+};
 
-document.querySelectorAll(".filter").forEach(item => {
-    let filteredSeafood = []
-    item.addEventListener('click', ()=>{
-        console.log(item.innerHTML)
-        switch(item.innerHTML){
-            case 'Fresh Fish':
-                console.log('here')  
-                filteredSeafood = seafood.filter((item)=> item.category==='fish' && item.type==='fresh') 
-                console.log(filteredSeafood)
-                displayFood(filteredSeafood);
-                break;
-            case 'Fresh Seafood':
-                console.log('here')  
-                filteredSeafood = seafood.filter((item)=> item.category==='seafood' && item.type==='fresh') 
-                console.log(filteredSeafood)
-                displayFood(filteredSeafood);
-                break;
-            case 'Frozen Fish':
-                console.log('here')  
-                filteredSeafood = seafood.filter((item)=> item.category==='fish' && item.type==='frozen') 
-                console.log(filteredSeafood)
-                displayFood(filteredSeafood);
-                break;
-            case 'Frozen Seafood':
-                console.log('here')  
-                filteredSeafood = seafood.filter((item)=> item.category==='seafood' && item.type==='frozen') 
-                console.log(filteredSeafood)
-                displayFood(filteredSeafood);
-                break;
-            case 'Prepared Fish':
-                console.log('here')  
-                filteredSeafood = seafood.filter((item)=> item.category==='fish' && item.type==='prepared') 
-                console.log(filteredSeafood)
-                displayFood(filteredSeafood);
-                break;
-            case 'Prepared Seafood':
-                console.log('here')  
-                filteredSeafood = seafood.filter((item)=> item.category==='seafood' && item.type==='prepared') 
-                console.log(filteredSeafood)
-                displayFood(filteredSeafood);
-                break;
-                                
-        }
-    })
-})
+document.querySelectorAll(".filter").forEach((item) => {
+  let filteredSeafood = [];
+  item.addEventListener("click", () => {
+    console.log(item.innerHTML);
+    switch (item.innerHTML) {
+      case "Fresh Fish":
+        console.log("here");
+        filteredSeafood = seafood.filter(
+          (item) => item.category === "fish" && item.type === "fresh"
+        );
+        console.log(filteredSeafood);
+        displayFood(filteredSeafood);
+        break;
+      case "Fresh Seafood":
+        console.log("here");
+        filteredSeafood = seafood.filter(
+          (item) => item.category === "seafood" && item.type === "fresh"
+        );
+        console.log(filteredSeafood);
+        displayFood(filteredSeafood);
+        break;
+      case "Frozen Fish":
+        console.log("here");
+        filteredSeafood = seafood.filter(
+          (item) => item.category === "fish" && item.type === "frozen"
+        );
+        console.log(filteredSeafood);
+        displayFood(filteredSeafood);
+        break;
+      case "Frozen Seafood":
+        console.log("here");
+        filteredSeafood = seafood.filter(
+          (item) => item.category === "seafood" && item.type === "frozen"
+        );
+        console.log(filteredSeafood);
+        displayFood(filteredSeafood);
+        break;
+      case "Prepared Fish":
+        console.log("here");
+        filteredSeafood = seafood.filter(
+          (item) => item.category === "fish" && item.type === "prepared"
+        );
+        console.log(filteredSeafood);
+        displayFood(filteredSeafood);
+        break;
+      case "Prepared Seafood":
+        console.log("here");
+        filteredSeafood = seafood.filter(
+          (item) => item.category === "seafood" && item.type === "prepared"
+        );
+        console.log(filteredSeafood);
+        displayFood(filteredSeafood);
+        break;
+    }
+  });
+});
 
-const product = document.querySelector(".product")
-console.log(product)
+const product = document.querySelector(".product");
+console.log(product);
 // .addEventListener('click', function (){
 //     document.location = 'seafood-detail.html'
 // })
